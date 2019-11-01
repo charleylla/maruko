ce3ae4825f4b5abbb4aebc531166d3f44fedb15e
Charley <charleylla@foxmail.com>
2019-11-01 11:17:55 +0800

    1. 组件驼峰；2.修改自动导入到 component.module 的 bug

--- packages/maruko-cli/bin/bootstrapGen.js
+++ packages/maruko-cli/bin/bootstrapGen.js
@@ -290,7 +290,7 @@ run:
 
     const storeAlias = this.resolveAliasPath();
     // 是否为 components 组件目录
-    const isComponentsDir = storeAlias.startsWith('~/solution/');
+    const isComponentsDir = storeAlias.startsWith('~/solution/components');
     if(isComponentsDir){
       const importStr = `export { ${this.fileNameCap}Component } from '${storeAlias}/${componentDestDirName}/${this.fileName}.component';`;
       const componentModuleContentAfterImport = componentModuleComponent.replace(packageEndReg, importStr + '\n' + packageEndStr)
--- packages/maruko-cli/template/@component/fname.component.store.ts.tpl
+++ packages/maruko-cli/template/@component/fname.component.store.ts.tpl
@@ -1,6 +1,6 @@
 import { action,observable } from 'mobx';
 
 export class %_StoreName_% {
-  @observable your_observable_props:string;
-  @action your_action_props:Function;
+  @observable yourObservableProps:string;
+  @action yourActionProps = () => {};
 }
